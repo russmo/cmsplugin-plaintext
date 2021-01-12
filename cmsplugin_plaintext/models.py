333c1e85c48bdfb77b6b77c12930be12dd01a25f
Russell Moffitt <russmo@coralhead.com>
2021-01-11 23:10:08 -0800

    Django 3.1 support, Django 2.2 support

--- cmsplugin_plaintext/models.py
+++ cmsplugin_plaintext/models.py
@@ -1,11 +1,9 @@
 from django.utils.translation import ugettext as _
-from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 
 from cms.models import CMSPlugin
 
 
-@python_2_unicode_compatible
 class CMSCharFieldPlugin(CMSPlugin):
     body = models.CharField(_('body'), max_length=500)
 
@@ -15,7 +13,6 @@ class CMSCharFieldPlugin(CMSPlugin):
     search_fields = ('body',)
 
 
-@python_2_unicode_compatible
 class CMSTextFieldPlugin(CMSPlugin):
     body = models.TextField(_('body'))
 
